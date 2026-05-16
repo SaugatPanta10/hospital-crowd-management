2d2dd8f927e11a01696f5f8b3313938e7d74789f
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-16 23:48:23 +0545

    Feat: Complete Day 3 filter logic challenge

---
+++ learning_materials/day03.py
@@ -0,0 +1,20 @@
+patient_queue = [
+    {"id": 101, "name": "Ram Bahadur", "status": "Pending"}, 
+    {"id": 102, "name": "Sita Thapa", "status": "Reviewed"}, 
+    {"id": 103, "name": "Gopal Sharma","status": "Pending" }
+]
+
+print ("--- Current Patient Queue Status ----")
+
+for patient in patient_queue: 
+    print (f"Patient ID: {patient['id']} | {patient['name']} | Status: {patient['status']}")
+
+
+""" this is a challenge task"""
+
+for patient in patient_queue: 
+    if patient['status'] == "Pending":
+        print (f"ATTENTION: Doctor review needed for {patient['name']} ")
+
+    else: 
+        print("skip")
