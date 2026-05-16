e0e9b8736749fcdd40a975b06736a3031d7dfe0a
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-17 01:14:14 +0545

    Feat: Create records app, register it, and design PatientReport model schema

---
+++ records/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class PatientReport(models.Model): 
+    patient_id = models.CharField(db_index = True, max_length = 20, unique = True)
+    patient_name = models.CharField(max_length = 100)
+    patient_phone = models.CharField(max_length=15)
+    test_name = models.CharField(max_length=100)
+
+    verdict = models.TextField(default= "Pending")
+
+    is_reviewed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Report {self.patient_id} - {self.patient_name}"
+
