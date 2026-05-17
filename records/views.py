21cb3364994e62b87cecbcd768aa99b8452a7536
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-17 15:58:13 +0545

    Feat: Complete Day 11 Dashbaord dynamic linking challenge

--- records/views.py
+++ records/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # django.shortcuts is a module that provides useful tools like rendering HTML templates , and render is a function used to return HTML pages
+from django.shortcuts import render, get_object_or_404 # django.shortcuts is a module that provides useful tools like rendering HTML templates , and render is a function used to return HTML pages
 from django.http import HttpResponse #django.http is a module that handles web requests and responses. HttpResponse sends text babck to the browser 
 from .models import PatientReport
 
@@ -7,10 +7,19 @@ def home_view(request):  #request is an object that contains the information sen
     context = {
         'patients': total_patients
         }
-    
     return render(request, 'records/home.html', context)
 
 def doctor_dashboard_view(request):
     all_reports = PatientReport.objects.all() #Take all records from PatientReport table and store them in a list
-    context = {'reports': all_reports}
+    context = {
+        'reports': all_reports
+        }
     return render(request, 'records/doctor_dashboard.html', context) #this context must always be a dictionary 
+
+def patient_detail_view(request, patient_id):
+    report = get_object_or_404(PatientReport, patient_id=patient_id)
+    context = {
+        'report' : report
+    }
+    return render(request, 'records/patient_detail.html', context)
+    
