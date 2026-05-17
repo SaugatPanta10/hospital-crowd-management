00af024bb982c1418fa3e2c19d5da8bfcbbcc97c
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-17 10:52:36 +0545

    Feat: Implement Day 8 views, URL routing, and doctor dashboard challenge

---
+++ records/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path #django.urls is a module to handle URL routing in Django, path is a function used to define URL Patterns 
+from . import views  # '.' means current folder, views means import views.py from the current folder
+
+# this is the list of all URL routes in this app. 
+urlpatterns = [ # it must be called exactly urlpatterns as this is a special django variable name 
+    path('', views.home_view, name = 'home'),
+    path('doctor/', views.doctor_dashboard_view, name = "doctor dashboard") 
+]
--- records/views.py
+++ records/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render # django.shortcuts is a module that provides useful tools like rendering HTML templates , and render is a function used to return HTML pages
+from django.http import HttpResponse #django.http is a module that handles web requests and responses. HttpResponse sends text babck to the browser 
 
 # Create your views here.
+def home_view(request):
+    return HttpResponse("Welcome to UpacharVerdict: The Hospital Crowd Management System. ")
+
+def doctor_dashboard_view(request):
+    return HttpResponse("Doctor Dashboard. You have 3 pending reports to review.") 
