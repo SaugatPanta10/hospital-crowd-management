8fbc3924400a11ddb5e6fea314d1255cb752c740
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-17 14:23:03 +0545

    Feat: Create template directories and render HTML pages for Day9

--- records/views.py
+++ records/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render # django.shortcuts is a module that provides
 from django.http import HttpResponse #django.http is a module that handles web requests and responses. HttpResponse sends text babck to the browser 
 
 # Create your views here.
-def home_view(request):
-    return HttpResponse("Welcome to UpacharVerdict: The Hospital Crowd Management System. ")
+def home_view(request):  #request is an object that contains the information sent by the browser to Django such as URL, user data,
+    return render(request, 'records/home.html')
 
 def doctor_dashboard_view(request):
-    return HttpResponse("Doctor Dashboard. You have 3 pending reports to review.") 
+    return render(request, 'records/doctor_dashboard.html')
+
