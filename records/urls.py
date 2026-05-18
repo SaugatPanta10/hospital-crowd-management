2b21994bd0ddd5a38c6e18f1a65cdba8294b24aa
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-18 21:56:57 +0545

    Feat: Refactor doctor dashboard template using base template

--- records/urls.py
+++ records/urls.py
@@ -4,6 +4,6 @@ from . import views  # '.' means current folder, views means import views.py fro
 # this is the list of all URL routes in this app. 
 urlpatterns = [ # it must be called exactly urlpatterns as this is a special django variable name 
     path('', views.home_view, name = 'home'),
-    path('doctor/', views.doctor_dashboard_view, name = "doctor dashboard"),
+    path('doctor/', views.doctor_dashboard_view, name = "doctor_dashboard"),
     path('patient/<str:patient_id>/', views.patient_detail_view, name = 'patient_detail')
 ]
