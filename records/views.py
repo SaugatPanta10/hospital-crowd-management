acc5ad578134a742da419d66d8b85c4243fc75d3
SaugatPanta10 <pantasaugat102@gmail.com>
2026-05-23 14:29:45 +0545

    Feat: Transform home page into patient search portal with database filtering

--- records/views.py
+++ records/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse #django.http is a module that handles web r
 from .models import PatientReport
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import PatientReport
-from .forms import PatientReportForm
+from .forms import PatientReportForm, DoctorVerdictForm
+
 
 def home_view(request):  #request is an object that contains the information sent by the browser to Django such as URL, user data,
     total_patients = PatientReport.objects.count()
@@ -53,3 +54,19 @@ def update_verdict_view (request, patient_id):
 
     context = {'form': form, 'report': report}
     return render(request, 'records/update_verdict.html', context)
+
+def home_view(request):
+    search_query = request.GET.get('query')
+    patient_result = None 
+
+    if search_query:
+        patient_result = PatientReport.objects.filter(patient_id=search_query).first()
+
+    total_count = PatientReport.objects.count()
+
+    context = {
+        'total_patients': total_count, 
+        'search_query': search_query, 
+        'patient_result': patient_result
+    }
+    return render(request, 'records/home.html', context)
